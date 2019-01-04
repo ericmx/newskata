68f4ce429f30d65059394c5671da57c570408de6
erictmay@me.com <erictmay@me.com>
2019-01-04 09:49:09 -0500

    current working copy

--- newskata_app/config/routes.rb
+++ newskata_app/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/about'
   root 'home#index'
 end
