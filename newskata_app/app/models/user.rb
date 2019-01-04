1274bb195a033e6792aeba582df4558f23ad2838
erictmay@me.com <erictmay@me.com>
2019-01-04 11:48:53 -0500

    added validation for name and email and uniquness, also added some navigation links to create an account

--- newskata_app/app/models/user.rb
+++ newskata_app/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :microposts
-  validates :name, presence: true
-  validates :email, presence: true
+  VALID_INPUT = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :name, presence: true, length: { maximum: 50 }, format: { with: VALID_INPUT }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_INPUT }, uniqueness: { case_sensitive: false }
 end
--- newskata_app/config/routes.rb
+++ newskata_app/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :microposts
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'static_pages/about'
+  get  '/about',   to: 'static_pages#about'
   root 'home#index'
+  get '/signup', to: 'users#new'
 end
