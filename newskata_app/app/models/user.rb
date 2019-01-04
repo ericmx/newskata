2b535cc87dfb7124592ff7c0c9e49164e0d68888
erictmay@me.com <erictmay@me.com>
2019-01-04 14:05:51 -0500

    finished for the moment

--- newskata_app/app/models/user.rb
+++ newskata_app/app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   has_many :microposts
+
+  before_save { self.email = email.downcase }
+
   VALID_INPUT = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
   validates :name, presence: true, length: { maximum: 50 }, format: { with: VALID_INPUT }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_INPUT }, uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 8 }
 end
